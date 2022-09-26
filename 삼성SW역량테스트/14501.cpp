cdd6e88e2300fcf25d81d0ea3d2bdf8ddf9b9f77
jeounpar <cdex6531@ajou.ac.kr>
2022-09-26 19:52:29 +0900

    solve 14501

--- 삼성SW역량테스트/14501.cpp
+++ 삼성SW역량테스트/14501.cpp
@@ -1,27 +1,41 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int t[21];
-int p[21];
+int t[16];
+int p[16];
 int n;
 int ans = 0;
-void solve(int day, int sum) {
-    if (day == n+1) {
-        if (ans < sum) ans = sum;
-        return;
-    }
-    if (day > n+1) {
-        return;
-    }
-    solve(day+1, sum);
-    solve(day+t[day], sum+p[day]);
+
+void solve(int day, int sum)
+{
+	if (day > n + 1) return;
+	if (day == n + 1) {
+		if (sum > ans) {
+			ans = sum;
+		}
+		return;
+	}
+	solve(day + 1, sum);
+	solve(day + t[day], sum + p[day]);
 }
-int main() {
-    cin >> n;
-    for (int i=1; i<=n; i++) {
-        cin >> t[i] >> p[i];
-    }
-    solve(1, 0);
-    cout << ans << '\n';
-    return 0;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		int a, b;
+		cin >> a >> b;
+		t[i] = a;
+		p[i] = b;
+	}
+	solve(1, 0);
+	cout << ans << '\n';
+	return 0;
 }
