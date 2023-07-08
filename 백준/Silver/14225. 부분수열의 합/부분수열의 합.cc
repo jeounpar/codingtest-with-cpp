6bae5c022c5190adf3b6e97189add77b668e2fe1
JeongKyu Park <34067179+jeounpar@users.noreply.github.com>
2023-07-09 01:38:46 +0900

    [Silver I] Title: 부분수열의 합, Time: 612 ms, Memory: 3976 KB -BaekjoonHub

---
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int n;
+vector<int> vc;
+bool ok[20 * 100000 + 3];
+
+void solve(int len, int idx, vector<int> tmp) {
+  if (tmp.size() == len) {
+    int sum = 0;
+    for (auto a : tmp)
+      sum += a;
+    ok[sum] = true;
+    return;
+  } else if (tmp.size() > len)
+    return;
+  for (int i = idx; i < n; i++) {
+    tmp.push_back(vc[i]);
+    solve(len, i + 1, tmp);
+    tmp.pop_back();
+  }
+}
+
+int main(void) {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  cin >> n;
+  int t;
+  for (int i = 0; i < n; i++) {
+    cin >> t;
+    vc.push_back(t);
+  }
+  sort(vc.begin(), vc.end());
+  for (int len = 1; len <= n; len++) {
+    solve(len, 0, {});
+  }
+  for (int i = 1; i <= 20 * 100000; i++) {
+    if (!ok[i]) {
+      cout << i;
+      return 0;
+    }
+  }
+  return 0;
+}
