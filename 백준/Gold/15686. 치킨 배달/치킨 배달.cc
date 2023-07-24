5efae8eb86b491e6f8f52b18df8f53daadc9b70d
JeongKyu Park <34067179+jeounpar@users.noreply.github.com>
2023-07-24 21:28:50 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -4,45 +4,50 @@ using namespace std;
 typedef long long ll;
 
 int n, m;
-int arr[51][51];
-vector<pair<int, int>> home;
-vector<pair<int, int>> ch;
 int ans = INT_MAX;
+vector<pair<int, int>> home;
+vector<pair<int, int>> chicken;
+
+void solve(int idx, vector<pair<int, int>> &vc) {
+  if (vc.size() == m) {
+    int sum = 0;
+    for (int i = 0; i < home.size(); i++) {
+      int t = INT_MAX;
+      for (auto a : vc) {
+        t = min(t,
+                abs(home[i].first - a.first) + abs(home[i].second - a.second));
+      }
+      sum += t;
+    }
+    ans = min(ans, sum);
+    return;
+  }
+  for (int i = idx; i < chicken.size(); i++) {
+    vc.push_back(chicken[i]);
+    solve(i + 1, vc);
+    vc.pop_back();
+  }
+}
 
 int main(void) {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
-  cout.tie(NULL);
+  cout.tie(0);
 
   cin >> n >> m;
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= n; j++) {
-      cin >> arr[i][j];
-      if (arr[i][j] == 1)
+  int t;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      cin >> t;
+      if (t == 1) {
         home.push_back({i, j});
-      else if (arr[i][j] == 2)
-        ch.push_back({i, j});
+      } else if (t == 2) {
+        chicken.push_back({i, j});
+      }
     }
   }
-  vector<int> vc;
-  for (int i = 0; i < ch.size() - m; i++)
-    vc.push_back(0);
-  for (int i = 0; i < m; i++)
-    vc.push_back(1);
-  do {
-    int dist_sum = 0;
-    for (auto h : home) {
-      int dist = INT_MAX;
-      for (int i = 0; i < vc.size(); i++) {
-        if (vc[i] == 1) {
-          int tmp = abs(h.first - ch[i].first) + abs(h.second - ch[i].second);
-          dist = min(tmp, dist);
-        }
-      }
-      dist_sum += dist;
-    }
-    ans = min(ans, dist_sum);
-  } while (next_permutation(vc.begin(), vc.end()));
+  vector<pair<int, int>> tmp;
+  solve(0, tmp);
   cout << ans;
   return 0;
 }
