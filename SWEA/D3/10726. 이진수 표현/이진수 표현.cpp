3a4adceb66d31594e1d679d932ffa482907ebbf7
JeongKyu Park <34067179+jeounpar@users.noreply.github.com>
2023-08-07 21:30:43 +0900

    [D3] Title: 이진수 표현, Time: 11 ms, Memory: 13,536 KB -BaekjoonHub

---
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+int answer = 0;
+
+void solve(int test_case) {
+  answer = 0;
+  int n, m;
+  cin >> n >> m;
+  string s;
+  for (int i = 0; i < n; i++) {
+    if (!(m & (1 << i))) {
+      cout << "#" << test_case << " OFF"
+           << "\n";
+      return;
+    }
+  }
+  cout << "#" << test_case << " ON"
+       << "\n";
+}
+
+int main(int argc, char **argv) {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int test_case;
+  int T;
+  cin >> T;
+  for (test_case = 1; test_case <= T; ++test_case) {
+    solve(test_case);
+  }
+  return 0;
+}
