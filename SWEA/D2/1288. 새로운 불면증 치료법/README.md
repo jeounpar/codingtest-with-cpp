53dacf08dcccd88d08ee586e5688496440383805
JeongKyu Park <34067179+jeounpar@users.noreply.github.com>
2023-08-07 21:17:57 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 10 ms, Memory: 9,480 KB -BaekjoonHub

---
+++ SWEA/D2/1288. 새로운 불면증 치료법/README.md
@@ -0,0 +1,11 @@
+# [D2] 새로운 불면증 치료법 - 1288 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV18_yw6I9MCFAZN) 
+
+### 성능 요약
+
+메모리: 9,480 KB, 시간: 10 ms, 코드길이: 716 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+unordered_set<char> us;
+
+void solve(int test_case) {
+  int answer = 0;
+  int n;
+  us.clear();
+  cin >> n;
+  int t = 0;
+  int k = n;
+  while (true) {
+    string s = to_string(k);
+    int slen = s.length();
+    for (int i = 0; i < slen; i++) {
+      us.insert(s[i]);
+    }
+    // cout << us.size() << "\n";
+    if (us.size() == 10)
+      break;
+    t++;
+    k = n * t;
+  }
+  cout << "#" << test_case << " " << k << "\n";
+}
+
+int main(int argc, char **argv) {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int test_case;
+  int T;
+  cin >> T;
+  for (test_case = 1; test_case <= T; ++test_case) {
+    solve(test_case);
+  }
+  return 0;
+}
