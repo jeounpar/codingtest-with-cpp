5003fcc7af6c01378c5932400de93aba912c9937
JeongKyu Park <34067179+jeounpar@users.noreply.github.com>
2023-08-22 22:26:47 +0900

    [Gold V] Title: ABCDE, Time: 256 ms, Memory: 22860 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,50 +1,56 @@
+import java.io.*;
 import java.util.*;
- 
-public class Main {    
-    
-    static int n;
-    static ArrayList<Integer>[] list;
-    static boolean[] visited;
-    
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
-        int n = scan.nextInt();
-        int m = scan.nextInt();
-        
-        list = new ArrayList[n];
-        for(int i = 0; i < n; i++) {
-            list[i] = new ArrayList<>();
-        }
-        
-        for(int i = 0; i < m; i++) {
-            int a = scan.nextInt();
-            int b = scan.nextInt();
-            list[a].add(b);
-            list[b].add(a);
-        }
-        
-        for(int i = 0; i < n; i++) {
-            visited = new boolean[n];
-            dfs(i, 0);
-        }
-        System.out.println(0);
-    }
-    
-    public static void dfs(int x, int len) {
-        if(len == 4) {
-            System.out.println(1);
-            System.exit(0);
-        }
-        
-        visited[x] = true;
-        for(int i = 0; i < list[x].size(); i++) {
-            int temp = list[x].get(i);
-            if(visited[temp] == false) {
-                visited[temp] = true;
-                dfs(temp, len + 1);
-                visited[temp] = false;
-            }
-        }
-    }
+
+public class Main {
+	static int answer = 0;
+	static int n, m;
+	static ArrayList<Integer>[] adj;
+	static boolean[] ok;
+
+	static void solve(int cur, int cnt) {
+		if (cnt == 5) {
+			answer = 1;
+			return;
+		}
+		for (Integer next : adj[cur]) {
+			if (ok[next]) continue;
+			ok[next] = true;
+			solve(next, cnt + 1);
+			ok[next] = false;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		adj = new ArrayList[n];
+		for (int i = 0; i < n; i++) {
+			adj[i] = new ArrayList<>();
+		}
+
+		int a, b;
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			adj[a].add(b);
+			adj[b].add(a);
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (answer == 1) break;
+			ok = new boolean[n];
+			ok[i] = true;
+			solve(i, 1);
+		}
+		bw.write(Integer.toString(answer));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
 }
