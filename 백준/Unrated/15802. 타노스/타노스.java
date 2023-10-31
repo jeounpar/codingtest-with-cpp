7f1802cbf595e6c8b5e693a1ea43946a6ba31d1b
JeongKyu Park <34067179+jeounpar@users.noreply.github.com>
2023-10-31 15:27:49 +0900

    [Unrated] Title: 타노스, Time: 76 ms, Memory: 11368 KB -BaekjoonHub

---
+++ 백준/Unrated/15802. 타노스/타노스.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int answer = 0;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		System.out.println("1");
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
